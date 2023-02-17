f19af658fb08f52d5ee63b7fc922dbf089528833
7bernie <bernardbassie@gmail.com>
2023-02-17 02:59:13 -0800

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
-
 /**
- * main - print the string in the put function
- *
- * Description: Using the main function
- * This program prints the size of various types on the computer it is compiled and run on 
- *Return: 0
+ * main - Entry point
+ * print size
+ * Return: 0
  */
 int main(void)
 {
-	char c;
-	int i;
-	long int li;
-	long long int lli
-	float f;
-
-	printf("size of a char: %ld byte(s)\n", sizeof(c));
-	printf("size of an int: %ld byte(s)\n", sizeof(i));
-	printf("size of a long int: %ld byte(s)\n", sizeof(li));
-	printf("size of a lolng long int: %ld byte(s)\n", sizeof(lli));
-	printf("size of a float: %ld byte(s)\n", sizeof(f));
-	return (0);
-}
-
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
+return (0);
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-script that generates the assembly code of a C code and save it in an output file.
-=======
-script that compiles a C file but does not link.
->>>>>>> 3a4a165538e506119b722ddb0f8c5ee48bbe7cc7
+0x00. C - Hello, World
