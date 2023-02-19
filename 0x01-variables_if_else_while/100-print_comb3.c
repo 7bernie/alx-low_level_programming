2d46d670e3a958de04e9fc4295c543508df8669b
7bernie <bernardbassie@gmail.com>
2023-02-18 20:14:25 -0800

    nventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,35 +1,30 @@
+#include <stdio.h>
+
 /**
- * main - prints out all the numbers between 00 and 99
- * with no two digits being the same
- * Return: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
  */
 
 int main(void)
 {
-	int i, j;
+	int n;
+	int i;
 
-	for (i = 48; i < 58; i++)
+	for (n = 0 ; n < 9 ; n++)
 	{
-		for (j = i; j < 58; j++)
-		{
-			if (i == j)
-			{
-				continue;
-			}
-
-			putchar(i);
-			putchar(j);
-
-			if (i == 56 && j == 57)
-			{
-				break;
-			}
-			else
+		i = n + 1;
+		do {
+			putchar('0' + n);
+			putchar('0' + i);
+			if (n < 8)
 			{
 				putchar(',');
-				putchar(' ');
+				putchar(32);
 			}
-		}
+			i++;
+		} while (i < 10);
 	}
 	putchar('\n');
-	ret
+	return (0);
+}
