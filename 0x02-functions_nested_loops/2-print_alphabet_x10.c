af0bb4cda560d7bfd15451c3d46b1e6920f164db
7bernie <bernardbassie@gmail.com>
2023-02-21 17:31:24 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
-* print_alphabet_x10 - function that print abc 10 times
-* 
-* Return : 0
-*/
+  * print_alphabet_x10 - function that print abc 10 times
+  * 
+  * Return : 0
+  */
+
 void print_alphabet_x10(void)
 {
 	char c, i;
@@ -16,4 +18,4 @@ void print_alphabet_x10(void)
 		}
 		_putchar('\n');
 	}
-}
+}}
