2e4ba4e14ae9b8e286b1b39d4bceeeb485995d08
7bernie <bernardbassie@gmail.com>
2023-02-21 21:24:02 -0800

    Jack Bauer is a federal agent

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * jack_bauer - function that prints every minute of the day of Jack Bauer starting
  * from 00:00 to 23:59, min loop counts mins while hour loop counts hours a nd resets mins
@@ -29,4 +28,5 @@ void jack_bauer(void)
 	}
 	hours++;
 	minutes = 0;
+}
 }
