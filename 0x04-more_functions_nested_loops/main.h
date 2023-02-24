70e83dfdb5ce5c47ec85a409b2ed24cbe2922fcc
7bernie <bernardbassie@gmail.com>
2023-02-24 01:30:10 -0800

    isupper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,17 +8,20 @@ main.h
 *int prototypes (int)
 */
 
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
 void print_numbers(void);
-aid priint_most_numbers(void);
+void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
-void print_trianngle(int size);
+void print_triangle(int size);
+int isPrime(long int n);
+long int biggestFactor(long int a);
 void print_number(int n);
-int_putchar(char);
-int_isupper(int);
-int_isdigit(int);
-Int mul(int, int);
+int numDigits(int n);
 
-#endif /* MAIN_H */
+#endif
