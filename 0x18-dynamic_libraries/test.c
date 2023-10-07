70e81d74bb28dee27c2268ee4b669ba989e22d3d
7bernie <bernardbassie@gmail.com>
2023-10-07 08:39:52 -0700

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/test.c
+++ 0x18-dynamic_libraries/test.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int myprintf(const char *format, ...)
+int myprintf(const int *format, ...)
 {
-        write(1, "9 8 10 24 75 - 9\n", 17);
-        write(1, "Congratulations, you win the Jackpot!\n", 55);
-        exit(EXIT_SUCCESS);
+	write(1, "9 8 10 24 75 - 9\n", 17);
+	write(1, "Congratulations, you win the Jackpot!\n", 38);
+	exit(EXIT_SUCCESS);
 }
