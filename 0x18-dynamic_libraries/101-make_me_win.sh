c6f4c628d5195919f7c7e0c825f5628c719f6a44
7bernie <bernardbassie@gmail.com>
2023-10-07 07:53:54 -0700

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,10 @@
-#!/bin/bash
-gcc shared -o myprintf.so fPIC test.c
-export LD_PRELOAD=./myprintf.so:$LD_LIBRARY_PATH
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int myprintf(const char *format, ...)
+{
+        write(1, "9 8 10 24 75 - 9\n", 17);
+        write(1, "Congratulations, you win the Jackpot!\n", 55);
+        exit(EXIT_SUCCESS);
+}
